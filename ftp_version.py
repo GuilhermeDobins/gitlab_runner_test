81d26da7996935dc2223f23b96edee451c569516
Guilherme Dobins <70792082+GuilhermeDobins@users.noreply.github.com>
2022-06-18 14:04:53 -0300

    Create ftp_version.py

---
+++ ftp_version.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import requests
+# find and save the current Github release
+html = str(
+    requests.get('https://github.com/restic/restic/releases/latest')
+    .content)
+index = html.find('href="/restic/restic/tree/v')
+github_version = html[index + 27:index + 33]
+file = open('github_version.txt', 'w')
+file.writelines(github_version)
+file.close()
+
+# find and save the current version on FTP server
+html = str(
+    requests.get(
+        'https://oplab9.parqtec.unicamp.br/pub/ppc64el/restic/'
+    ).content)
+index = html.rfind('restic-')
+ftp_version = html[index + 7:index + 13]
+file = open('ftp_version.txt', 'w')
+file.writelines(ftp_version)
+file.close()
+
+# find and save de oldest version on the FTP server
+index = html.find('restic-')
+if (html[index + 12] == '\"'):
+    delete = html[index + 7:index + 12]
+else:
+    delete = html[index + 7:index + 13]
+delete = html[index + 7:index + 12]
+file = open('delete.txt', 'w')
+file.writelines(delete)
+file.close()
