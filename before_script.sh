ba411e4ba71fda997e724e768c607fd8b8332cad
Guilherme Dobins <70792082+GuilhermeDobins@users.noreply.github.com>
2022-06-19 18:34:39 -0300

    Update before_script.sh

--- before_script.sh
+++ before_script.sh
@@ -6,3 +6,7 @@ apt-get install -y build-essential python3 python3-pip tar wget lftp git
 python3 -m pip install requests
 printf "set ssl:verify-certificate false" > ~/.lftprc
 git clone https://github.com/GuilhermeDobins/gitlab_runner_test
+git clone https://github.com/rpsene/goconfig
+cd goconfig
+source ./go.sh install
+cp -p /usr/local/go/bin/go /usr/local/bin
