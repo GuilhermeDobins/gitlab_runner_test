e6a6cb9aef86daa98e796270503badbade671800
Guilherme Dobins <70792082+GuilhermeDobins@users.noreply.github.com>
2022-06-19 17:50:07 -0300

    Update before_script.sh

--- before_script.sh
+++ before_script.sh
@@ -3,5 +3,6 @@
 apt-get update
 apt-get install -y rpm
 apt-get install -y build-essential python3 python3-pip tar wget lftp git
+python3 -m pip install requests
 printf "set ssl:verify-certificate false" > ~/.lftprc
 git clone https://github.com/GuilhermeDobins/gitlab_runner_test
